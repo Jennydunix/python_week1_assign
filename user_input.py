946a4adba8c6fc23750786d75f8bcb4bc84b736d
jennydunix <jennifer.o.odunze@gmail.com>
2024-03-01 14:35:13 +0100

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('What is your name?')
+age = input('What is your age?')
+location = input('What is your location?')
+
+print('Hello!, Your name is ', name,'.', 'Your are ', age, 'old', 'and live in  ', location)
